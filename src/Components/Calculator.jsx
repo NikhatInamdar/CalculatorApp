5c4c9199aa5f79c03b387208107d63279bfa8424
Nikhat Inamdar <nikinamdar9494@gmail.com>
2024-11-29 18:51:56 -0800

    my first commit

---
+++ src/Components/Calculator.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import './Calculator.css'
+
+const Calculator = () =>{
+   const [input ,setInput] = useState("")
+
+   const showdata = (data) =>{
+       setInput(input + data)
+   }
+
+
+  const clearAllData = () =>{
+    setInput('')
+  }
+  
+  const clearLastEntry = () =>{
+    setInput(input.slice(0,-1))
+  }
+
+  const solveEqu = () =>{
+    try{
+        setInput(eval(input).toString())
+    }
+    catch{
+        setInput('Error')
+    }
+    console.log(typeof eval(input))
+  }
+
+
+    return(
+        <div>
+            <h2>Calculator App</h2>
+            <div className="calculator">
+                <input type="text" className="calculator-screen"  value={input}/>
+
+                <div className='calculator-buttons'>
+                    <button className='cal-btn' onClick= {()=>  showdata('1')}>1</button>
+                    <button className='cal-btn' onClick= {()=>  showdata('2')}>2</button>
+                    <button className='cal-btn' onClick= {() => showdata('3')}>3</button>
+                    <button className='cal-btn' onClick= {() => showdata('+')}>+</button>
+
+                    <button className='cal-btn' onClick= {()=>  showdata('4')}>4</button>
+                    <button className='cal-btn' onClick= {()=>  showdata('5')}>5</button>
+                    <button className='cal-btn' onClick= {()=>  showdata('6')}>6</button>
+                    <button className='cal-btn' onClick= {()=>  showdata('-')}>-</button>
+
+
+                    <button className='cal-btn' onClick= {()=>  showdata('7')} >7</button>
+                    <button className='cal-btn' onClick= {()=>  showdata('8')}>8</button>
+                    <button className='cal-btn' onClick= {()=>  showdata('9')}>9</button>
+                    <button className='cal-btn' onClick= {()=>  showdata('*')}>*</button>
+
+
+                    <button className='cal-btn' onClick= {()=>  showdata('0')}>0</button>
+                    <button className='cal-btn' onClick= {()=>  showdata('.')}>.</button>
+                    <button className='cal-btn' onClick= {()=>  showdata('/')}>/</button>
+                    <button className='cal-btn' onClick= {solveEqu}>=</button>
+                </div>
+
+
+                <div className='clear-all'>
+                    <button className='cal-btn' onClick={clearAllData}>clear</button>
+                    <button  className='cal-btn' onClick={clearLastEntry}>&#9003;</button>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+export default Calculator;
